5d2f55ac919b2efddb64be02d0f06bfcb86387b9
Mia Rivera <riverami@iupui.edu>
2017-06-29 11:09:44 -0400

    "Favorite button causes line background Color change"

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 console.log("link test")
 const app = {
     intit(selectors) {
+        this.flicks = []
         this.max = 0
         this.list = document.querySelector(selectors.listSelect)
 
@@ -12,10 +13,15 @@ const app = {
     handleSubmit(ev) {
         ev.preventDefault()
         const form = ev.target
-        const flick = {
+        let flick = {
             name: form.flickName.value,
             id: this.max + 1,
+            favStatus: false,
         }
+
+        //add flick to flicks array
+        this.flicks.push(flick)
+        console.log(this.flicks)
 
         const listItem = this.renderListItem(flick)
         this.list.appendChild(listItem)
@@ -25,10 +31,61 @@ const app = {
 
     renderListItem(flick) {
         const item = document.createElement('li')
+        item.id = this.max
         item.textContent = flick.name
+
+        //TODO add a favorite button to each item, causes a change of appearance
+        //     also affects array (change fav status)
+        const favButton = document.createElement('BUTTON')
+        favButton.id = this.max
+        favButton.innerHTML = "<img src=\"https://upload.wikimedia.org/wikipedia/commons/7/79/Operation_Upshot-Knothole_-_Badger_001.jpg\"/>"
+        favButton.style.width = "2rem"
+        favButton.style.height = "1rem"
+        item.appendChild(favButton)
+
+        favButton.addEventListener('click', function() {
+            const line = document.getElementById(favButton.id)
+            
+            if(line.style.backgroundColor === "yellow") {
+                line.style.backgroundColor = "white"
+            } else {
+                line.style.backgroundColor = "yellow"
+            }
+            // let status = this.flicks[favButton.id - 1].favStatus
+            // if(status === false) {
+            //     status = true
+            //     console.log(this.flicks)
+            // } else {
+            //     status = false
+            // }
+        })
+
+        //TODO add a remove button to each item, remove from list and array
+
         return item
     },
+
+    // changeFav() {
+    //     console.log(this.flicks)
+    //     console.log(this.favButton.id)
+    //     let status = this.flicks[this.favButton.id - 1].favStatus
+    //     if(status === false) {
+    //         status = true
+    //         console.log(this.flicks)
+    //     } else {
+    //         status = false
+    //     }
+
+    //     // console.log(document.querySelector(this.max))
+    //     if(status === true) {
+    //         //const button = querySelector("b" + this.max + "")
+    //         //console.log(button)
+    //         //button.style.backgroundColor = "yellow"
+    //     }
+    // },
 }
+
+
 app.intit({
     formSelect: 'form#flick-form', 
     listSelect: '#flick-list',
